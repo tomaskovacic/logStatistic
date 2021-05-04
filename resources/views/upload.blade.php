bfdb4fad060d2d684d4db1dce8d5f3e63fb5eebe
tomas <tomas.kovacic1@gmail.com>
2021-05-04 15:58:41 +0200

    Project created, file upload and stored in storage

---
+++ resources/views/upload.blade.php
@@ -0,0 +1,7 @@
+<h1>Upload File</h1>
+
+<form action="upload" method="POST" enctype="multipart/form-data">
+    @csrf
+    <input type="file" name="file"> <br> <br>
+    <button type="submit">Upload File</button>
+</form>
