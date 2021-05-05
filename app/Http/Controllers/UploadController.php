454825ddea5d0cd5018602157a7a0fb4dd9ca2cd
Samo Faric <samofaric99@gmail.com>
2021-05-05 11:55:32 +0200

    Added reading of file contents
    Reading contents of file after upload it

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
     // 
-    function index(Request $req){
-        return $req->file('file')->store('logs');
+    function index(Request $req): string
+    {
+        try {
+            return Storage::disk('local')->get($req->file('file')->store('logs'));
+        } catch (FileNotFoundException $e) {
+            return "File not found";
+        }
     }
 }
