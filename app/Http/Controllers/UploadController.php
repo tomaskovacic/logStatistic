3fe58b0722f4e9773bb959057ed42fb962fdd48f
tomas <tomas.kovacic1@gmail.com>
2021-05-05 13:04:04 +0200

    Update reading

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -6,13 +6,27 @@ use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
+
 class UploadController extends Controller
 {
     // 
     function index(Request $req): string
     {
         try {
-            return Storage::disk('local')->get($req->file('file')->store('logs'));
+            $path = $req->file('file')->store('logs');
+            $path = trim($path,"logs/");
+            $text = "app\\logs\\";
+            $text .= $path; 
+            $file = fopen(storage_path($text), "r");
+            $array=[];
+            $i = 0;
+            while(!feof($file)) {
+                $array[$i] = fgets($file);
+                $i++;
+                
+            }
+            return $array[4];
+            
         } catch (FileNotFoundException $e) {
             return "File not found";
         }
--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -4,17 +4,15 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
 <style>
-
 </style>
 </head>
 
 
 <body>
-    <div>
+    <div style="text-align:center">
     <h1 class="display-4">Log Statistics</h1>
-    </div>
+    </div> <br>
     <div>
         <table class="table table-striped">
             <thead>
