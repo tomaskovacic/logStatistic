235e5e1b6d5a10794a39a0b6d80e24b81bb804ac
tomas <tomas.kovacic1@gmail.com>
2021-05-05 14:22:32 +0200

    Removed duplicates from the file

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -15,25 +15,26 @@ class UploadController extends Controller
     {
         try {
             $path = $req->file('file')->store('logs');
-            $path = trim($path,"logs/");
+            $path = trim($path, "logs/");
             $end_path = "app\\logs\\";
-            $end_path .= $path; 
+            $end_path .= $path;
             $file = fopen(storage_path($end_path), "r");
-            $array=[];
-            $array_of_results=[];
+            $array = [];
+            $array_without_duplicates = [];
+            $array_of_results = [];
             $line_counter = 0;
             $info_counter = 0;
             $debug_counter = 0;
             $error_counter = 0;
-            while(!feof($file)) {
+            while (!feof($file)) {
                 $array[$line_counter] = fgets($file);
-                if(str_contains($array[$line_counter], "local.INFO" )){
+                if (str_contains($array[$line_counter], "local.INFO")) {
                     $info_counter++;
                 }
-                if(str_contains($array[$line_counter], "local.DEBUG" )){
+                if (str_contains($array[$line_counter], "local.DEBUG")) {
                     $debug_counter++;
                 }
-                if(str_contains($array[$line_counter], "local.ERROR" )){
+                if (str_contains($array[$line_counter], "local.ERROR")) {
                     $error_counter++;
                 }
                 $line_counter++;
@@ -42,9 +43,16 @@ class UploadController extends Controller
             $array_of_results[1] = $debug_counter;
             $array_of_results[2] = $error_counter;
 
-        
-            return $array_of_results;
             
+            $array_without_duplicates = array_unique($array, SORT_STRING );
+            //dd(count($array_without_duplicates)); 
+            //dd(count($array)); 
+            $counter2=3;
+            foreach($array_without_duplicates as $item) {
+                $array_of_results[$counter2] = $item;
+                $counter2++;
+            }
+            return $array_of_results;
         } catch (FileNotFoundException $e) {
             return "File not found";
         }
