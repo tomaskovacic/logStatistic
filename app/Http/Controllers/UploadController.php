9cf9434ea28e2c94091615afb969ded71fc7ffc8
tomas <tomas.kovacic1@gmail.com>
2021-05-05 13:33:04 +0200

    ERROR, DEBUG, INFO counter added in text file

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -9,23 +9,41 @@ use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
+
     // 
-    function index(Request $req): string
+    function index(Request $req): array
     {
         try {
             $path = $req->file('file')->store('logs');
             $path = trim($path,"logs/");
-            $text = "app\\logs\\";
-            $text .= $path; 
-            $file = fopen(storage_path($text), "r");
+            $end_path = "app\\logs\\";
+            $end_path .= $path; 
+            $file = fopen(storage_path($end_path), "r");
             $array=[];
-            $i = 0;
+            $array_of_results=[];
+            $line_counter = 0;
+            $info_counter = 0;
+            $debug_counter = 0;
+            $error_counter = 0;
             while(!feof($file)) {
-                $array[$i] = fgets($file);
-                $i++;
-                
+                $array[$line_counter] = fgets($file);
+                if(str_contains($array[$line_counter], "local.INFO" )){
+                    $info_counter++;
+                }
+                if(str_contains($array[$line_counter], "local.DEBUG" )){
+                    $debug_counter++;
+                }
+                if(str_contains($array[$line_counter], "local.ERROR" )){
+                    $error_counter++;
+                }
+                $line_counter++;
             }
-            return $array[4];
+            $array_of_results[0] = $info_counter;
+            $array_of_results[1] = $debug_counter;
+            $array_of_results[2] = $error_counter;
+
+        
+            return $array_of_results;
             
         } catch (FileNotFoundException $e) {
             return "File not found";
