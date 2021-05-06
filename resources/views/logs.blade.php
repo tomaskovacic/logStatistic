0692a02eb30498593349c963bdb931db6110aaae
Samo Faric <samofaric99@gmail.com>
2021-05-06 09:22:52 +0200

    Update logs.blade.php
    Added pagination

--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -4,8 +4,15 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<style>
-</style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#table').DataTable({"ordering": false });
+        $('.dataTables_length').addClass('bs-select');
+    });
+</script>
 </head>
 
 
@@ -19,7 +26,7 @@
     <h5> Number of info: {{$array_of_results[2]}}</h5> <br>
     </div>
     <div class="table-responsive" >
-        <table class="table table-striped" style="table-layout:fixed; width: 100%;">
+        <table id="table" class="table table-striped" style="table-layout:fixed; width: 100%;">
             <thead class="thead-dark">
                 <tr>
                     <th scope="col" style="text-align:center; word-wrap: break-word;">Errors</th>
