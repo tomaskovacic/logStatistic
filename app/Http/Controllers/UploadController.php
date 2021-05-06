98bd46bd5a5edad6e8ee4bc5e4dd09811587629a
tomas <tomas.kovacic1@gmail.com>
2021-05-06 12:32:21 +0200

    Data brake into date, error name and error description and display it

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -19,7 +19,7 @@ class UploadController extends Controller
             while (!feof($file)) {
                 $array[$lineCounter] = fgets($file);
                 yield $array[$lineCounter];
-                $lineCounter++;
+                $lineCounter++;//TODO: peginacija 
             }
 
         }
@@ -32,6 +32,10 @@ class UploadController extends Controller
             $file = fopen(storage_path($endPath), "r");
             $array = [];
             $arrayOfResults = [];
+            $date= [];
+            $errorName = [];
+            $errorDesc = [];
+
             $lineCounter = 0;
             $infoCounter = 0;
             $debugCounter = 0;
@@ -39,6 +43,12 @@ class UploadController extends Controller
 
             $generator = get_line($file);
             foreach ($generator as $line) {
+                $array[$lineCounter] = $line;
+                $lineCounter++;
+            }
+            $counter1=0;
+            $arrayWithoutDuplicates = array_unique($array, SORT_STRING );
+            foreach ($arrayWithoutDuplicates as $line) {
                 if (str_contains($line, "local.INFO")) {
                     $infoCounter++;
                 }
@@ -48,21 +58,31 @@ class UploadController extends Controller
                 if (str_contains($line, "local.ERROR")) {
                     $errorCounter++;
                 }
-                $array[$lineCounter] = $line;
-                $lineCounter++;
+                if(str_contains($line, "local.ERROR") or str_contains($line, "local.DEBUG") or str_contains($line, "local.INFO")) {
+                    $date[$counter1] = substr($line, 1, 19);
+                    $errorDesc[$counter1] = substr($line,34);
+                    if(str_contains($line, "local.INFO")){
+                        $errorName[$counter1] = substr($line,28, 4);
+                    }else {
+                        $errorName[$counter1] = substr($line,28, 5);
+                    }
+                    $counter1++;
+                }
+
             }
+
             $arrayOfResults[0] = $infoCounter;
             $arrayOfResults[1] = $debugCounter;
             $arrayOfResults[2] = $errorCounter;
 
+            $arrayFinal= array($date, $errorName, $errorDesc);
 
-            $arrayWithoutDuplicates = array_unique($array, SORT_STRING );
             $counter2=3;
             foreach($arrayWithoutDuplicates as $item) {
                 $arrayOfResults[$counter2] = $item;
                 $counter2++;
             }
-            return view('logs',['arrayOfResults'=>$arrayOfResults]);
+            return view('logs',['arrayFinal'=>$arrayFinal], ['arrayOfResults'=>$arrayOfResults] );
         } catch (FileNotFoundException $e) {
             return "File not found";
         }
--- resources/views/logs.blade.php
+++ resources/views/logs.blade.php
@@ -2,51 +2,55 @@
 <html lang="en">
 
 <head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
-<script>
-    $(document).ready(function () {
-        $('#table').DataTable({"ordering": false });
-        $('.dataTables_length').addClass('bs-select');
-    });
-</script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js">
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#table').DataTable({
+                "ordering": false
+            });
+            $('.dataTables_length').addClass('bs-select');
+        });
+
+    </script>
 </head>
 
 
 <body>
     <div style="text-align:center">
-    <h1 class="display-4">Log Statistics</h1>
+        <h1 class="display-4">Log Statistics</h1>
     </div> <br>
     <div style="margin-left: 30px;">
-    <h5> Number of errors: {{$arrayOfResults[0]}}</h5>
-    <h5> Number of debugs: {{$arrayOfResults[1]}}</h5>
-    <h5> Number of info: {{$arrayOfResults[2]}}</h5> <br>
+        <h5> Number of errors: {{ $arrayOfResults[0] }}</h5>
+        <h5> Number of debugs: {{ $arrayOfResults[1] }}</h5>
+        <h5> Number of info: {{ $arrayOfResults[2] }}</h5> <br>
     </div>
-    <div class="table-responsive" >
+    <div class="table-responsive">
         <table id="table" class="table table-striped" style="table-layout:fixed; width: 100%;">
             <thead class="thead-dark">
                 <tr>
-                    <th scope="col" style="text-align:center; word-wrap: break-word;">Errors</th>
+                    <th scope="col" style="word-wrap: break-word;">Date</th>
+                    <th scope="col" style="word-wrap: break-word;">Error name</th>
+                    <th scope="col" style="word-wrap: break-word;">Error description</th>
                 </tr>
             </thead>
             <tbody>
-                @php 
-                unset($arrayOfResults[0]);
-                unset($arrayOfResults[1]);
-                unset($arrayOfResults[2]);
-                @endphp
-                @foreach($arrayOfResults as $item)
-                <tr>
-                    <td style="word-wrap: break-word;">{{$item}}</td>
-                </tr>
-                @endforeach
+                @for ($i = 0; $i < sizeof($arrayFinal[0]); $i++)
+                    <tr>
+                        @for ($j = 0; $j < count($arrayFinal); $j++)
+                            <td style="word-wrap: break-word;">{{ $arrayFinal[$j][$i] }}</td>
+                        @endfor
+                    </tr>
+                @endfor
+                
             </tbody>
         </table>
     </div>
-                <br><br>
+    <br><br>
 
 
 </body>
