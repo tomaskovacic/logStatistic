9889fdb960b8b5842c8ca73f6e17e01d238fbf18
tomas <tomas.kovacic1@gmail.com>
2021-05-06 13:18:30 +0200

    Table data fixed

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Php;
 
 class UploadController extends Controller
 {
@@ -19,9 +19,8 @@ class UploadController extends Controller
             while (!feof($file)) {
                 $array[$lineCounter] = fgets($file);
                 yield $array[$lineCounter];
-                $lineCounter++;//TODO: peginacija 
+                $lineCounter++; //TODO: peginacija 
             }
-
         }
 
         try {
@@ -32,7 +31,7 @@ class UploadController extends Controller
             $file = fopen(storage_path($endPath), "r");
             $array = [];
             $arrayOfResults = [];
-            $date= [];
+            $date = [];
             $errorName = [];
             $errorDesc = [];
 
@@ -46,8 +45,8 @@ class UploadController extends Controller
                 $array[$lineCounter] = $line;
                 $lineCounter++;
             }
-            $counter1=0;
-            $arrayWithoutDuplicates = array_unique($array, SORT_STRING );
+            $counter1 = 0;
+            $arrayWithoutDuplicates = array_unique($array, SORT_STRING);
             foreach ($arrayWithoutDuplicates as $line) {
                 if (str_contains($line, "local.INFO")) {
                     $infoCounter++;
@@ -58,31 +57,40 @@ class UploadController extends Controller
                 if (str_contains($line, "local.ERROR")) {
                     $errorCounter++;
                 }
-                if(str_contains($line, "local.ERROR") or str_contains($line, "local.DEBUG") or str_contains($line, "local.INFO")) {
+                if (str_contains($line, "local.ERROR") or str_contains($line, "local.DEBUG") or str_contains($line, "local.INFO")) {
                     $date[$counter1] = substr($line, 1, 19);
-                    $errorDesc[$counter1] = substr($line,34);
-                    if(str_contains($line, "local.INFO")){
-                        $errorName[$counter1] = substr($line,28, 4);
-                    }else {
-                        $errorName[$counter1] = substr($line,28, 5);
+                    if (str_contains($line, '{"')) {
+                        if (str_contains($line, "local.INFO")) {
+                            $text = trim($line, substr($line, 0, 32));
+                            $errorDesc[$counter1] = substr($text, 0, strpos($text, '{"'));
+                        } else {
+                            $text = trim($line, substr($line, 0, 32));
+                            $errorDesc[$counter1] = substr($text, 0, strpos($text, '{"'));
+                        }
+                    } else {
+                        $errorDesc[$counter1] = substr($line, 34);
+                    }
+                    if (str_contains($line, "local.INFO")) {
+                        $errorName[$counter1] = substr($line, 28, 4);
+                    } else {
+                        $errorName[$counter1] = substr($line, 28, 5);
                     }
                     $counter1++;
                 }
-
             }
 
             $arrayOfResults[0] = $infoCounter;
             $arrayOfResults[1] = $debugCounter;
             $arrayOfResults[2] = $errorCounter;
 
-            $arrayFinal= array($date, $errorName, $errorDesc);
+            $arrayFinal = array($date, $errorName, $errorDesc);
 
-            $counter2=3;
-            foreach($arrayWithoutDuplicates as $item) {
+            /*$counter2 = 3;
+            foreach ($arrayWithoutDuplicates as $item) {
                 $arrayOfResults[$counter2] = $item;
                 $counter2++;
-            }
-            return view('logs',['arrayFinal'=>$arrayFinal], ['arrayOfResults'=>$arrayOfResults] );
+            }*/
+            return view('logs', ['arrayFinal' => $arrayFinal], ['arrayOfResults' => $arrayOfResults]);
         } catch (FileNotFoundException $e) {
             return "File not found";
         }
